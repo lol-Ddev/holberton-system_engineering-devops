2b2bdceab230622df06c4c4f346f0af07af049e9
imac_esneider <sneidergv@gmail.com>
2021-08-15 12:10:19 +0000

    100-puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,7 @@
 # using Puppet to make changes to our configuration file
 
 class { 'ssh::server':
+    path    => '/etc/ssh/ssh_config',
     options => {
           'PasswordAuthentication' => 'no',
           'IdentityFile'           => '~/.ssh/holberton'
