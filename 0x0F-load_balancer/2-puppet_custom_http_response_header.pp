e93fd2a84b8a9f051b6c432bd96a5dbe44dc3905
imac_esneider <sneidergv@gmail.com>
2021-08-24 20:22:06 -0500

    2-puppet_custom

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -8,6 +8,6 @@ exec { 'setting up nginx':
                 echo "Holberton School" | sudo tee /var/www/html/index.nginx-debian.html;
                 sudo sed -i "/server_name _;/ a \\\trewrite ^/redirect_me https://www.google.com permanent;" /etc/nginx/sites-available/default
                 sudo sed -i "11i \\\t add_header X-Served-By $HOSTNAME always;\n" /etc/nginx/nginx.conf;
-                sudo service nginx restart'
+                sudo service nginx restart',
   provider => shell,
 }
