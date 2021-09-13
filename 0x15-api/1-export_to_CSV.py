d38b4dcf3a0f13c5356a6740c5e55a87ae8a5d7f
imac_esneider <sneidergv@gmail.com>
2021-09-13 15:47:51 -0500

    protect 1-export

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -8,22 +8,24 @@ import csv
 import requests
 from sys import argv
 
-user_id = argv[1]
-api_url = 'https://jsonplaceholder.typicode.com'
-with requests.Session() as s:
-    employee = s.get('{}/users/{}'.format(api_url, user_id)).json()
-    to_dos = s.get('{}/todos/'.format(api_url)).json()
+if __name__ == '__main__':
+    user_id = argv[1]
+    api_url = 'https://jsonplaceholder.typicode.com'
+    with requests.Session() as s:
+        employee = s.get('{}/users/{}'.format(api_url, user_id)).json()
+        to_dos = s.get('{}/todos/'.format(api_url)).json()
 
-    todo_list = []
-    completed_tasks = 0
-    for task in to_dos:
-        if task['userId'] == int(user_id):
-            todo_list.append(task)
-            if task['completed'] is True:
-                completed_tasks = completed_tasks + 1
+        todo_list = []
+        completed_tasks = 0
+        for task in to_dos:
+            if task['userId'] == int(user_id):
+                todo_list.append(task)
+                if task['completed'] is True:
+                    completed_tasks = completed_tasks + 1
 
-    fieldnames = ['userId', 'name']
-    with open('{}.csv'.format(argv[1]), 'w') as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        for task in todo_list:
-            writer.writerow([task['userId'], employee['name'], task['completed'], task['title']])
+        fieldnames = ['userId', 'name']
+        with open('{}.csv'.format(argv[1]), 'w') as csvfile:
+            writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+            for task in todo_list:
+                writer.writerow([task['userId'], employee['name'],
+                                task['completed'], task['title']])
